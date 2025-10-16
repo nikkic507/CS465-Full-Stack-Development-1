e946785a52af9e6ab96180b190bd5410a8a248be
Nicholle Caudy <nikkic507@gmail.com>
2025-10-16 18:02:41 -0400

    Module 7 completed baseline

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -7,6 +7,7 @@ import { Trip } from '../models/trip';
 import { TripDataService } from '../services/trip-data.service'; 
 
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -23,7 +24,8 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
     ) {
     console.log('trip-listing constructor');
   }
@@ -57,5 +59,12 @@ export class TripListingComponent implements OnInit {
     this.getStuff();
 
     }
+
+    public isLoggedIn()
+    {
+      return this.authenticationService.isLoggedIn();
+    }
+
+
   }
 
