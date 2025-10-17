b48dbac3c1a59bb344fe242e9600a34c40b16d64
Nicholle Caudy <nikkic507@gmail.com>
2025-10-17 16:33:27 -0400

    Module 7 completed baseline

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var handlebars = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -14,12 +16,6 @@ var newsRouter = require('./app_server/routes/news');
 var roomsRouter = require('./app_server/routes/rooms');
 var apiRouter = require('./app_api/routes/index');
 
-var handlebars = require('hbs');
-
-
-//Bring in the database
-require('./app_api/models/db');
-
 var app = express();
 
 // view engine setup
@@ -29,16 +25,28 @@ app.set('views', path.join(__dirname,'app_server', 'views'));
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 app.set('view engine', 'hbs');
 
+//Bring in the database
+require('./app_api/models/db');
+
+//Bring in env environment file, environment variable capabilities
+require('dotenv').config();
+
+//Wire in authentication module
+var passport = require('passport');
+require('./app_api/config/passport');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
 
 //Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -53,6 +61,14 @@ app.use('/news', newsRouter);
 app.use('/rooms', roomsRouter);
 app.use('/api', apiRouter);
 
+//catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
